2c4fe65f0a8661d70c3574ef0b0dc6d8f1401d68
Dylan Ryals <dylan.k.ryals@gmail.com>
2024-12-05 08:11:12 +0000

    cbh upload

--- barcodes.R
+++ barcodes.R
@@ -1,4 +1,5 @@
 library(dplyr)
+library(readxl)
 
 #read file
 bc = read.delim("barcodes/Keyfile_bee_bag13.txt")
@@ -26,10 +27,49 @@ write.table(bc %>% filter(GBS_Plate == 2) %>% select(FullSampleName, Barcode),
             file = "barcodes/P2barcodes-full.txt",
             row.names = F, col.names = F, quote = F, sep = "\t")
 
-
 #pull names
 bc$name = gsub("^23[-](.*)[d|w].*", "\\1", bc$FullSampleName)
 unique(bc$name)
+
+
+
+
+### CBH barcodes
+  filename = "barcodes/23CBH_dilution plates_barcoded.xlsx"
+  
+  bc = data.frame(well = NA, barcodes = NA, sample = NA, plate = NA)
+  
+  sheets = excel_sheets(filename)
+  for(s in sheets){
+    
+    bc.part = rbind(read_excel(filename, sheet = s, range = "B1:D49"),
+                    read_excel(filename, sheet = s, range = "H1:J49"))
+    bc.part$plate = s
+    
+    colnames(bc.part) = c("well", "barcodes", "sample", "plate")
+    
+    bc = rbind(bc, bc.part)
+    
+  }
+
+  bc = bc %>% filter(!is.na(sample)) %>% 
+    rename(colony = sample) 
+  bc$sample = NA
+
+  #create unique sample names
+  for(c in unique(bc$colony)){
+    bc$sample[bc$colony == c] = 
+      paste0(bc$colony[bc$colony == c], "_", 1:sum(bc$colony == c))
+  }
+  
+#write out
+  for (PLATE in unique(bc$plate)){
+    p = gsub("Plate ", "", PLATE)
+    filename = paste0("barcodes/23CBH_formatted/23CBH_", p, ".txt")
+    write.table(bc %>% filter(plate == PLATE),
+                filename,
+                row.names = F, quote = F, col.names = F, sep = '\t')
+  }
 
 
 
---
+++ cbh_gbs_pipeline.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+# FILENAME: cbh_gbs_pipeline.sh
+
+#SBATCH -A bharpur
+#SBATCH --ntasks=20
+#SBATCH --mem-per-cpu=10G
+#SBATCH --time=1-08:00:00
+#SBATCH --job-name gbs_pipeline
+#SBATCH --output=/home/dryals/ryals/honeybee-gbs/outputs/cbh.out
+#SBATCH --error=/home/dryals/ryals/honeybee-gbs/outputs/cbh.out
+
+#Dylan Ryals 05 DEC 2024
+#last edited 
+
+date
+
+#iPyrad
+#this takes around 30 hours on 120 cores...
+
+module load anaconda use.own
+conda activate ipyrad
+
+# #first-time activation
+#     conda create -n ipyrad
+#     conda activate ipyrad
+#     conda install ipyrad -c conda-forge -c bioconda
+# 
+# #run R to output barcodes
+#     Rscript --vanilla --silent barcodes.R
+# 
+
+P=1 #plate number
+
+echo "setup plate $P ... "
+
+
+
+
+#copy parameter file into scratch directory
+    #TODO: create param files with vim or something
+    mkdir -p $CLUSTER_SCRATCH/gbs/23CBH/23CBH_${P}
+    cp params-23CBH_${P}.txt $CLUSTER_SCRATCH/gbs/23CBH/23CBH_${P}
+
+
+#rename fastqs: _R1_ and _R2_ required in filename!!!
+    #TODO: ensure all dirs and fastq are named the same...
+#     cd data/CBH2023
+#     if[! dir 23CBH_${P}]
+#         mv CBH_${P} 23CBH_${P}
+#         cd 23CBH_${P}
+#         mv CBH_${P}_1.fq.gz 23CBH_${P}_R1_.fastq.gz
+#         mv CBH_${P}_2.fq.gz 23CBH_${P}_R2_.fastq.gz
+#     fi
+#     
+    
+
+echo "starting ipyrad..."
+    cd $CLUSTER_SCRATCH/gbs/23CBH/23CBH_${P}
+#     #first step: demultiplexing
+      ipyrad -p params-bag13p1.txt -s 1 -c $SLURM_NTASKS -d -f --MPI
+#     ipyrad -p params-bag13p2.txt -s 1 -c $SLURM_NTASKS -d -f --MPI
+#     
+#     ipyrad -m bag13 params-bag13p1.txt params-bag13p2.txt
+# 
+#     #attempt the rest of the steps
+#     ipyrad -p params-bag13.txt -s 234567 -c $SLURM_NTASKS -d -f --MPI
+    
+    #drop samples that failed to assemble
+    ipyrad -p params-bag13.txt -b bag13-final - 23-II18w09
+    
+    #output final results
+    ipyrad -p params-bag13-final.txt -s 7 -c $SLURM_NTASKS -d -f --MPI
+
+    
+####
+echo "DONE"
+date
+
+
+
+
+
+
+
+
+    
+
