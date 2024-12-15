62b8367c1df62eb62927acaa3b6e371f3d2fa1d7
Dylan Ryals <dylan.k.ryals@gmail.com>
2024-12-15 10:53:21 +0000

    cbh ipyrad

--- cbh_merge.sh
+++ cbh_merge.sh
@@ -3,8 +3,8 @@
 # FILENAME: cbh_merge.sh
 
 #SBATCH -A bharpur
-#SBATCH --ntasks=4
-#SBATCH --mem-per-cpu=10G
+#SBATCH --ntasks=22
+#SBATCH --mem-per-cpu=8G
 #SBATCH --time=2-00:00:00
 #SBATCH --job-name cbh_merge
 #SBATCH --output=/home/dryals/ryals/honeybee-gbs/outputs/merge.out
@@ -22,33 +22,33 @@ module load anaconda use.own
 conda activate ipyrad
 
 echo "-------------"
-
-echo "merging plates..."
-    #gather completed files
-    cd $CLUSTER_SCRATCH/gbs/23CBH
-        mkdir -p testmerge4
-        cd testmerge4
-        #create var with all plate names
-        ls ../*/*.json
-        #...
-        echo -n "" > mergep.txt
-        #for i in 1 2 3 4 5 6 7 8 9 10 11 12 17 18 19 20 21 22 23 30 31
-        for i in 22 30
-        do
-            echo -n "../23CBH_${i}/params-23CBH_${i}.txt " >> mergep.txt
-        done 
-            echo "" >> mergep.txt
-        mp=$( cat mergep.txt )
-        
-    #create merged param file
-    ipyrad -m testmerge4 $mp
-    
-    #edit if needed...
-        
+# 
+# echo "merging plates..."
+#     #gather completed files
+#     cd $CLUSTER_SCRATCH/gbs/23CBH
+#         mkdir -p testmerge4
+#         cd testmerge4
+#         #create var with all plate names
+#         ls ../*/*.json
+#         #...
+#         echo -n "" > mergep.txt
+#         for i in 1 2 3 4 5 6 7 8 9 10 11 12 17 18 19 20 21 22 23 30 31
+#         #for i in 22 30
+#         do
+#             echo -n "../23CBH_${i}/params-23CBH_${i}.txt " >> mergep.txt
+#         done 
+#             echo "" >> mergep.txt
+#         mp=$( cat mergep.txt )
+#         
+#     #create merged param file
+#     ipyrad -m testmerge4 $mp
+#     
+#     #edit if needed...
+#         
 echo "launching ipyrad..."
 #     ipyrad -p params-merged.txt -s 34567 -c $SLURM_NTASKS -d -f --MPI
-    #cd $CLUSTER_SCRATCH/gbs/23CBH/varcalltest
-    ipyrad -p params-testmerge4.txt -s 567 -c $SLURM_NTASKS -d -f --MPI
+    cd $CLUSTER_SCRATCH/gbs/23CBH/varcalltest
+    ipyrad -p params-varcalltest.txt -s 567 -c $SLURM_NTASKS -d -f --MPI
         #only 5 plates
         #s3 alone may take 4.5 days with only 4 tasks... 
             #try 20 tasks * 6GB
